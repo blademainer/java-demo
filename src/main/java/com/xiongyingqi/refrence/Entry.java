8841dcd41e1103ac130eeab81e25ec8172c8b966
blademainer <blademainer@gmail.com>
2016-03-25 21:01:02 +0800

    refrence

---
+++ src/main/java/com/xiongyingqi/refrence/Entry.java
@@ -0,0 +1,26 @@
+package com.xiongyingqi.refrence;
+
+/**
+ * @author xiongyingqi
+ * @version 2016-03-25 16:54
+ */
+public class Entry {
+    private String key;
+    private String value;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
