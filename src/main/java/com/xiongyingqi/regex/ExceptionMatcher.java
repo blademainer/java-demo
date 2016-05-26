fb9387fae28d460f8f1d4105ef6053af0eeb7fa3
blademainer <blademainer@gmail.com>
2016-05-26 16:29:14 +0800

    regex

---
+++ src/main/java/com/xiongyingqi/regex/ExceptionMatcher.java
@@ -0,0 +1,31 @@
+package com.xiongyingqi.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by xiongyingqi on 16-5-26.
+ */
+public class ExceptionMatcher {
+    public static final String  regex   = "Caused\\sby:\\s.*\\n+(\\s+at\\s+.*?\\n+)*";
+    public static final Pattern PATTERN = Pattern.compile(regex, Pattern.MULTILINE);
+
+    static {
+    }
+
+    public static void main(String[] args) {
+        String test = "Caused by: java.lang.reflect.InvocationTargetException\n"
+                + "\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\n"
+                + "\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:39)\n"
+                + "\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:25)\n"
+                + "\tat java.lang.reflect.Method.invoke(Method.java:597)\n"
+                + "\tat java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:439)\n"
+                + "\tat java.util.concurrent.FutureTask$Sync.innerRunAndReset(FutureTask.java:31\n"
+                + "\t... 14 more";
+        System.out.println(test);
+        Matcher matcher = PATTERN.matcher(test);
+        if (matcher.find()) {
+            System.out.println("find====" + matcher.group());
+        }
+    }
+}
